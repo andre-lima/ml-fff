1a127b0b7d32fddc08c642249231e77cf4ccb28c
Andre Dantas <andre.dantas.lima@gmail.com>
2018-11-11 14:18:21 -0500

    finish video 2

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const randomPoints = range(0, 100).map(x => ({
   y: random(0, Y_MAX, true)
 }));
 
+
 const randomWeights = ({
   x: random(-1, 1, true),
   y: random(-1, 1, true)
@@ -17,7 +18,7 @@ const randomWeights = ({
 
 const team = point => point.x > point.y ? 1 : -1;
 
-const guess =  (weights, point) => {
+const guess = (weights, point) => {
   const sum = point.x * weights.x + point.y * weights.y
   const team = sum >= 0 ? 1 : -1;
   return team;
@@ -31,10 +32,36 @@ const testTrain = () => {
 function train(weights, point, actualTeam) {
   const guessResult = guess(weights, point);
   const error = actualTeam - guessResult;
-  return guessResult;
+  return {
+    x: weights.x + (point.x * error),
+    y: weights.y + (point.y * error)
+  }
 }
 
 const testGuess = guess(randomWeights, {x: 300, y: 400});
+
+const trainedWeights = () => {
+  const ex1 = { x: 721, y: 432 };
+  const ex2 = { x: 211, y: 122 };
+  const ex3 = { x: 328, y: 833 };
+  const ex4 = { x: 900, y: 400 };
+  const ex5 = { x: 21, y: 32 };
+  const ex6 = { x: 511, y: 2 };
+  const ex7 = { x: 128, y: 733 };
+  const ex8 = { x: 900, y: 4 };
+  let newWeights;
+
+  newWeights = train(randomWeights, ex1, team(ex1));
+  newWeights = train(newWeights, ex2, team(ex2));
+  newWeights = train(newWeights, ex3, team(ex3));
+  newWeights = train(newWeights, ex4, team(ex4));
+  newWeights = train(newWeights, ex5, team(ex5));
+  newWeights = train(newWeights, ex6, team(ex6));
+  newWeights = train(newWeights, ex7, team(ex7));
+  newWeights = train(newWeights, ex8, team(ex8));
+
+  return newWeights;
+};
 
 
 const svgOutput = `<svg width="${X_MAX}" height="${Y_MAX}"> \
@@ -43,12 +70,10 @@ ${randomPoints.map(point =>
      cx="${point.x}"
      cy="${point.y}"
      r="3"
-     fill="${guess(randomWeights, point) === -1 ? 'blue' : 'red'}" />`
+     fill="${guess(trainedWeights(), point) === -1 ? 'blue' : 'red'}" />`
 )}
 <line x1="0" x2="${X_MAX}" y1="0" y2="${Y_MAX}" stroke="red" />
 </svg>` ;
-
-
 
 const canvas = document.getElementById('canvas');
 canvas.innerHTML = svgOutput; 
